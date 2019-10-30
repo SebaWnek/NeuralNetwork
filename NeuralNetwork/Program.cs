49619953eb13da534f395ce5c473a4b6ed02232f
SebaWnek <47484373+SebaWnek@users.noreply.github.com>
2019-10-30 02:56:07 +0100

    Created working network

--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -10,10 +10,31 @@ namespace NeuralNetwork
     {
         static void Main(string[] args)
         {
-            Network network = new Network(4, 3, 3, 2);
-            network.CalculateNetwork(new float[] { 3, 2, 4, 1 });
-            float[] result = network.GetOutputs();
-            foreach (float i in result) Console.WriteLine(i);
+            Random random = new Random();
+            Network network = new Network(2, 2, 1);
+            //network.Layers[network.Layers.Length - 1].Function = Functions.LeakyReLU;
+            //network.Layers[network.Layers.Length - 1].Derivative = Functions.DLeakyReLU;
+            for (int i = 0; i < 1000000; i++)
+            {
+                float[] inputs = new float[] { random.Next(11), random.Next(1, 11) };
+                int res = (int)(inputs[0] + inputs[1]);
+                float[] output = new float[1];
+                output[0] = res;
+                //Console.WriteLine(inputs[0] + " * " + inputs[1] + " = " + res);
+                //foreach (float j in output) Console.Write(j + ", ");
+                //Console.WriteLine();
+                network.TrainNetwork(inputs, output);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                float[] input = new float[] { random.Next(11), random.Next(11) };
+                network.CalculateNetwork(input);
+                float[] result = network.GetOutputs();
+                Console.Write(input[0] + " + " + input[1] + " = ");
+                foreach (float j in result) Console.Write((j) + " => ");
+                Console.WriteLine((int)Math.Round(result[0]) == (input[0] + input[1]));
+                Console.WriteLine();
+            }
         }
     }
 
@@ -27,31 +48,96 @@ namespace NeuralNetwork
         }
     }
 
+    static class Functions
+    {
+        public static float[] TanH(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = (float)Math.Tanh(input[i]);
+            }
+            return output;
+        }
+
+        public static float[] DTanH(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = 1 - input[i] * input[i];
+            }
+            return output;
+        }
+
+        public static float[] LeakyReLU(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[i] >= 0 ? input[i] : 0.01f * input[i];
+            }
+            return output;
+        }
+
+        public static float[] DLeakyReLU(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[i] >= 0 ? 1 : 0.01f;
+            }
+            return output;
+        }
+
+        public static float[] Sigmoid(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = (float)(1 / (1 + Math.Pow(Math.E, -1 * input[i])));
+            }
+            return output;
+        }
+
+        public static float[] DSigmoid(float[] input)
+        {
+            float[] output = new float[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[i] * (1 - input[i]);
+            }
+            return output;
+        }
+    }
+
     class Layer
     {
-        static readonly int minRand = -1;
+        static readonly int minRand = 0;
         static readonly int maxRand = 1;
-        static readonly private float learningRate = 0.0001f;
+        static readonly private float learningRate = 0.001f;
         static readonly Random random = new Random();
-        Func<float[], float[]> Function;
-        Func<float[], float[]> Derivative;
+        public Func<float[], float[]> Function { get; set; }
+        public Func<float[], float[]> Derivative { get; set; }
 
         public float[,] Weights { get; set; }
         public float[,] DWeights { get; set; }
         public float[] Biases { get; set; }
+        public float[] DBiases { get; set; }
         public float[] Outputs { get; set; }
         public float[] Gammas { get; set; }
 
         public Layer(int prevCount, int count)
         {
             Biases = new float[count];
+            DBiases = new float[count];
             Outputs = new float[count];
             Gammas = new float[count];
             Weights = new float[count, prevCount];
             DWeights = new float[count, prevCount];
             InitializeLayer();
-            Function = TanH;
-            Derivative = DTanH;
+            Function = Functions.LeakyReLU;
+            Derivative = Functions.DLeakyReLU;
         }
 
         private void InitializeLayer()
@@ -101,50 +187,38 @@ namespace NeuralNetwork
             }
         }
 
-        private float[] TanH(float[] input)
+        private void CalculateBiasDerivatives()
         {
-            float[] output = new float[input.Length];
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 0; i < DBiases.Length; i++)
             {
-                output[i] = (float)Math.Tanh(input[i]);
+                DBiases[i] = Gammas[i];
             }
-            return output;
         }
 
-        private float[] DTanH(float[] input)
+        private void CalculateGammas(Layer nextLayer)
         {
-            float[] output = new float[input.Length];
-            for (int i = 0; i < input.Length; i++)
-            {
-                output[i] = 1 - input[i] * input[i];
-            }
-            return output;
-        }
-
-        public void CalculateGammas(Layer nextLayer)
-        {
-            float[] gamma;
+            float[] gamma = new float[Gammas.Length];
             float[] derivatives = Derivative(Outputs);
             for (int i = 0; i < Gammas.Length; i++)
             {
-                for(int j = 0; j < nextLayer.Gammas.Length; j++)
+                for (int j = 0; j < nextLayer.Gammas.Length; j++)
                 {
-                    gamma[i] += nextLayer.Gammas[j] * nextLayer.Weights[j,i];
+                    gamma[i] += nextLayer.Gammas[j] * nextLayer.Weights[j, i];
                 }
                 gamma[i] *= derivatives[i];
             }
         }
 
-        public void CalculateOutputGammas(float[] desiredOutputs)
+        private void CalculateGammas(float[] desiredOutputs)
         {
             float[] derivatives = Derivative(Outputs);
-            for(int i = 0; i < Gammas.Length; i++)
+            for (int i = 0; i < Gammas.Length; i++)
             {
                 Gammas[i] = (Outputs[i] - desiredOutputs[i]) * derivatives[i];
             }
         }
 
-        public void CalculateWeightsDerivatives(float[] previousOutputs)
+        private void CalculateWeightsDerivatives(float[] previousOutputs)
         {
             for (int i = 0; i < Gammas.Length; i++)
             {
@@ -155,15 +229,62 @@ namespace NeuralNetwork
             }
         }
 
-        public void CalculateNewWeights()
+        private void UpdateWeights()
         {
-            for(int i = 0; i < Weights.GetLength(0); i++)
+            for (int i = 0; i < Weights.GetLength(0); i++)
             {
-                for(int j = 0; j < Weights.GetLength(1); j++)
+                for (int j = 0; j < Weights.GetLength(1); j++)
                 {
-                    Weights[i,j] -= learningRate * DWeights[i,j];
+                    Weights[i, j] -= learningRate * DWeights[i, j];
                 }
             }
+        }
+
+        private void UpdateBiases()
+        {
+            for (int i = 0; i < Biases.Length; i++)
+            {
+                Biases[i] -= learningRate * DBiases[i];
+            }
+        }
+        /// <summary>
+        /// Calculates output layer
+        /// </summary>
+        /// <param name="desiredOutput">Desired outputs</param>
+        /// <param name="previousLayer">Preceeding layer</param>
+        public void CalculateBackPropagationDeltas(float[] desiredOutput, Layer previousLayer)
+        {
+            CalculateGammas(desiredOutput);
+            CalculateWeightsDerivatives(previousLayer.Outputs);
+            CalculateBiasDerivatives();
+        }
+        /// <summary>
+        /// Calculates hidden layers except for first one
+        /// </summary>
+        /// <param name="nextLayer">Following layer</param>
+        /// <param name="previousLayer">Preceeding layer</param>
+        public void CalculateBackPropagationDeltas(Layer nextLayer, Layer previousLayer)
+        {
+            CalculateGammas(nextLayer);
+            CalculateWeightsDerivatives(previousLayer.Outputs);
+            CalculateBiasDerivatives();
+        }
+        /// <summary>
+        /// Calculates first hidden layer, next after input layer
+        /// </summary>
+        /// <param name="nextLayer">Following layer</param>
+        /// <param name="input">Network inputs</param>
+        public void CalculateBackPropagationDeltas(Layer nextLayer, Input input)
+        {
+            CalculateGammas(nextLayer);
+            CalculateWeightsDerivatives(input.Inputs);
+            CalculateBiasDerivatives();
+        }
+
+        public void UpdateLayer()
+        {
+            UpdateWeights();
+            UpdateBiases();
         }
     }
 
@@ -212,14 +333,25 @@ namespace NeuralNetwork
 
         private void CalculateBackPropagation(float[] desiredOutput)
         {
-            Layers[Layers.Length - 1].CalculateOutputGammas(desiredOutput);
-            Layers[Layers.Length - 1].CalculateWeightsDerivatives(Layers[Layers.Length - 2].Outputs);
-            for (int i = Layers.Length - 2; i > 0; i--)
+            for(int i = Layers.Length - 1; i >= 0; i--)
             {
-                Layers[i].CalculateGammas(Layers[i+1]);
-                i > 0 ? Layers[i].CalculateWeightsDerivatives(Layers[i - 1].Outputs) : Layers[i].CalculateWeightsDerivatives(Input.Inputs);
+                if (i == Layers.Length - 1)
+                {
+                    Layers[i].CalculateBackPropagationDeltas(desiredOutput, Layers[i - 1]);
+                }
+                else if (i > 0)
+                {
+                    Layers[i].CalculateBackPropagationDeltas(Layers[i + 1], Layers[i - 1]);
+                }
+                else
+                {
+                    Layers[i].CalculateBackPropagationDeltas(Layers[i + 1], Input);
+                }
             }
-            foreach(Layer layer in Layers) layer.CalculateNewWeights();
+            for(int i = Layers.Length - 1; i >= 0; i--)
+            {
+                Layers[i].UpdateLayer();
+            }
         }
 
         public void TrainNetwork(float[] inputs, float[] desiredOutput)
